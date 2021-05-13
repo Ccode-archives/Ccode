15568601061e407dbbdd67f288e8f1488da88fb3
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-05-13 11:46:46 -0700

    Add files via upload

---
+++ Ccode.py
@@ -0,0 +1,50 @@
+#imports and settings
+import os
+running = True
+os.system("pwd")
+os.system("cp ~/lang/scripts/main.js ~/lang/")
+
+#js writing command
+def js(out):
+    f = open("main.js", "a")
+    f.write("\n")
+    f.write(out)
+    f.close()
+
+#unknown command message
+def NU():
+    print("unknown command")
+
+#loop start
+while running:
+    #input
+    inp = input("Ccode language >> ")
+    #print
+    if inp.startswith("print "):
+        println = inp.replace("print ", "")
+        out = "console.log(" + println + ");"
+        js(out)
+    #run
+    elif inp == "run":
+        running = False
+    #if statements
+    elif inp.startswith("if ") and inp.endswith(" {"):
+        out = inp.replace("if ", "")
+        out = out.replace(" {", "")
+        js("if (" + out + ") {")
+    #end brackets
+    elif inp == "}":
+        js(inp)
+    #variables
+    elif inp.find("=") > -1 or inp.find(" = ") > -1:
+        out = inp
+        if inp.find("input") > -1:
+            out = inp.replace("input", "prompt('>>')")
+        js(out + ";")
+    else:
+        NU()
+#end of loop
+
+#run commands
+os.system("./run")
+os.system("rm main.js")
