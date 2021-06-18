03f7847e74a51f75d561fb05647d9d79b9947337
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-06-18 13:54:14 -0400

    Add files via upload

---
+++ examples/main.cc
@@ -0,0 +1,7 @@
+// errors are skipped by the compiler ("error" can be replaced by any word, try it!).  This may cause a node error so see if the compiler finds errors.
+error
+func test() {
+	error
+	print 'hi'
+}
+test()
