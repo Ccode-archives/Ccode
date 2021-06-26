005e550bef92c2aeb0f6fbbeb3b6af1c1411249a
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-06-25 20:45:55 -0500

    Update Ccode.py

--- Ccode.py
+++ Ccode.py
@@ -19,10 +19,10 @@ try:
     file.close()
 except:
     print("\n\n\n\ngiven file missing, aborting\n\n\n\n")
-    os.system("rm main.js")
     os.system("rm -r node_modules")
     os.system("rm package.json")
     os.system("rm package-lock.json")
+    os.system("rm main.js")
 
 #js writing command
 def js(out):
